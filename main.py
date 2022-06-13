93fc3bfcb51fb14db9f44f021eb6392204304bc1
graham katana <graykatanakenny@gmail.com>
2022-06-13 22:03:34 +0200

    Emails sent

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,BackgroundTasks
 from blog import Models
 from db.database import engine, get_db
 from routes import blog, user, auth
+import os
+from utils.emails import send_email_background,send_email_async
+
 
 app = FastAPI()
 Models.Base.metadata.create_all(engine)
 app.include_router(auth.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+
+@app.get('/send-email/asynchronous')
+async def send_email_asynchronous():
+    await send_email_async('Greetings', 'graykatanakenny@gmail.com')
+    return  'sent'
+
 
 get_deb = get_db()
---
+++ utils/emails.py
@@ -0,0 +1,30 @@
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from dotenv import load_dotenv
+# load_dotenv('.env')
+async def send_email_async(subject: str, email_to: str):
+    conf = ConnectionConfig(
+        MAIL_USERNAME="",
+        MAIL_PASSWORD="",
+        MAIL_FROM="",
+        MAIL_PORT=465,
+        MAIL_SERVER="",
+        MAIL_TLS=False,
+        MAIL_SSL=True,
+        USE_CREDENTIALS=True,
+        VALIDATE_CERTS=True,
+        TEMPLATE_FOLDER='./templates/emails'
+    )
+
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        body="Good morning",
+        subtype='html',
+    )
+
+    fm = FastMail(conf)
+
+    await fm.send_message(message, template_name='email.html')
+
+
+
